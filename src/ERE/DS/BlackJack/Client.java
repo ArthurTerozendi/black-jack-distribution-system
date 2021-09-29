7119841e3da201903029c6a1a0b997d096bba60d
Arthur Terozendi <arthurterozendi@gmail.com>
2021-09-28 22:18:37 -0300

    update code

--- src/ERE/DS/BlackJack/Client.java
+++ src/ERE/DS/BlackJack/Client.java
@@ -6,33 +6,35 @@ import java.net.*;
 public class Client {
     public static void main(String args[]) throws Exception
     {
+        boolean stop = false;
+        while (!stop) {
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            DatagramSocket clientSocket = new DatagramSocket();
 
-        DatagramSocket clientSocket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("localhost");
 
-        InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
 
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
+            String sentence = inFromUser.readLine();
+            System.out.println(sentence);
+            sendData = sentence.getBytes();
 
-        String sentence = inFromUser.readLine();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 
-        sendData = sentence.getBytes();
+            clientSocket.send(sendPacket);
 
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-        clientSocket.send(sendPacket);
+            clientSocket.receive(receivePacket);
+            if (sentence.equalsIgnoreCase("n")) stop = true;
+            String modifiedSentence = new String(receivePacket.getData());
 
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            System.out.println("FROM SERVER:" + modifiedSentence);
 
-        clientSocket.receive(receivePacket);
-
-        String modifiedSentence = new String(receivePacket.getData());
-
-        System.out.println("FROM SERVER:" + modifiedSentence);
-
-        clientSocket.close();
+            clientSocket.close();
+        }
 
     }
 }
--- src/ERE/DS/BlackJack/ServerUDP.java
+++ src/ERE/DS/BlackJack/ServerUDP.java
@@ -2,6 +2,9 @@ package ERE.DS.BlackJack;
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class ServerUDP {
     public static void main(String args[]) throws Exception
@@ -12,23 +15,61 @@ public class ServerUDP {
         byte[] receiveData = new byte[1024];
         byte[] sendData  = new byte[1024];
 
-        while(true)
+        ArrayList<String> cards = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            cards.add("A");
+            cards.add("2");
+            cards.add("3");
+            cards.add("4");
+            cards.add("5");
+            cards.add("6");
+            cards.add("7");
+            cards.add("8");
+            cards.add("9");
+            cards.add("10");
+            cards.add("J");
+            cards.add("Q");
+            cards.add("K");
+        }
+        Collections.shuffle(cards);
+        int aux = 0;
+        int total = 0;
+        boolean stop = false;
+        while(!stop)
         {
-
             DatagramPacket receivePacket =
                     new DatagramPacket(receiveData, receiveData.length);
 
             serverSocket.receive(receivePacket);
 
             String sentence = new String(receivePacket.getData());
-
+            System.out.println(sentence);
             InetAddress IPAddress = receivePacket.getAddress();
 
             int port = receivePacket.getPort();
+            String send = "";
+            if (aux == 0) {
+                aux++;
+                String card = cards.remove(cards.size() - 1);
+                total += convertToInt(card);
+                send += card + ", ";
+                card = cards.remove(cards.size() - 1);
+                total += convertToInt(card);
+                send += card + " - Total: " + total;
+            }
+            else {
+                if (sentence.equals("y")) {
+                    String card = cards.remove(cards.size() - 1);
+                    total += convertToInt(card);
+                    send = card + " - Total: " + total;
+                }
+                else {
+                    send = "Total: " + total;
+                    stop = true;
+                }
+            }
 
-            String capitalizedSentence = sentence.toUpperCase();
-
-            sendData = capitalizedSentence.getBytes();
+            sendData = send.getBytes();
 
             DatagramPacket sendPacket =
                     new DatagramPacket(sendData, sendData.length, IPAddress,
@@ -36,5 +77,11 @@ public class ServerUDP {
 
             serverSocket.send(sendPacket);
         }
+
+        serverSocket.close();
+    }
+
+    private static int convertToInt(String card) {
+        return card.equals("J") || card.equals("Q") || card.equals("K") ? 10 : Integer.parseInt(card);
     }
 }
